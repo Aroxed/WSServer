48bbb5ff88d3b1ed9741f1a803fcd975d445cac1
Aroxed <petrashenko@gmail.com>
2024-04-16 15:02:50 +0300

    Initial commit

---
+++ chat/consumers.py
@@ -0,0 +1,15 @@
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        pass
+
+    def chat_message(self, event):
+        pass
